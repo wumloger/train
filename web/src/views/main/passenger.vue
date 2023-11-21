a75a9e31ba58953d1d89f49e392abbc1f27bcddb
ray <656482519@qq.com>
2023-11-21 15:39:33 +0800

    乘车人新增界面开发

---
+++ web/src/views/main/passenger.vue
@@ -0,0 +1,66 @@
+<template>
+    <a-button type="primary" @click="showModal">新增</a-button>
+    <a-modal 
+        v-model:visible="visible" 
+        title="乘车人" 
+        @ok="handleOk"
+        ok-text="确认"
+        cancel-text="取消"
+    >
+        <a-form
+            :model="passenger"
+            :label-col="{span:4}"
+            :wrapper-col="{span:20}">
+          <a-form-item label="姓名">
+            <a-input v-model:value="passenger.name"></a-input>
+          </a-form-item>
+          <a-form-item label="身份证">
+            <a-input v-model:value="passenger.idCard"></a-input>
+          </a-form-item>
+          <a-form-item label="类型">
+            <a-select v-model:value="passenger.type">
+              <a-select-option value="1">成人</a-select-option>
+              <a-select-option value="2">儿童</a-select-option>
+              <a-select-option value="3">学生</a-select-option>
+            </a-select>
+          </a-form-item>
+
+        </a-form>
+    </a-modal>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+import { notification } from 'ant-design-vue';
+import axios from 'axios';
+const visible = ref(false);
+
+const passenger = reactive({
+    id: undefined,
+    memberId: undefined,
+    name: undefined,
+    idCard: undefined,
+    type: undefined,
+    createTime: undefined,
+    updateTime: undefined
+})
+const showModal = () => {
+    visible.value = true;
+}
+const handleOk = () => {
+    axios.post("/member/passenger/save", passenger).then((res) => {
+        let data = res.data;
+        if (data.success) {
+            notification.success({ description: '保存成功!' });
+            visible.value = false;
+        } else {
+            notification.error({ description: data.message });
+        }
+    })
+    visible.value = false;
+}
+</script>
+
+<style scoped>
+
+</style>
