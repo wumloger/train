a98f5750e7dce0caf3d8dd2ade6cb86d6d3a8c98
ray <656482519@qq.com>
2023-11-21 19:23:11 +0800

    制作 Service 生成器

--- generator/src/main/java/top/wml/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/top/wml/train/generator/gen/ServerGenerator.java
@@ -1,5 +1,6 @@
 package top.wml.train.generator.gen;
 
+import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -7,6 +8,7 @@ import org.dom4j.io.SAXReader;
 import top.wml.train.generator.util.FreemarkerUtil;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +17,8 @@ import java.util.Map;
  */
 public class ServerGenerator {
     static String servicePath = "[module]/src/main/java/top/wml/train/[module]/service/";
+    static String serverPath = "[module]/src/main/java/top/wml/train/[module]/";
+
     static String pomPath = "generator/pom.xml";
 
     static {
@@ -27,11 +31,12 @@ public class ServerGenerator {
         // 比如generator-config-member.xml，得到module = member
         String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
         System.out.println("module: " + module);
-        servicePath = servicePath.replace("[module]", module);
+        serverPath = serverPath.replace("[module]", module);
         // new File(servicePath).mkdirs();
-        System.out.println("servicePath: " + servicePath);
+        System.out.println("servicePath: " + serverPath);
 
-        // 读取table节点
+
+        // 读取 table 节点
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
         System.out.println(table);
@@ -39,12 +44,12 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
-        // 示例：表名 mqxu_test
+        // 示例：表名 wml_test
         // Domain = MqxuTest
         String Domain = domainObjectName.getText();
-        // domain = mqxuTest
+        // domain = wmlTest
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
-        // do_main = mqxu_test
+        // do_main = wml_test
         String do_main = tableName.getText().replaceAll("_", "-");
 
         // 组装参数
@@ -54,8 +59,20 @@ public class ServerGenerator {
         param.put("do_main", do_main);
         System.out.println("组装参数：" + param);
 
-        FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(servicePath + Domain + "Service.java", param);
+        //FreemarkerUtil.initConfig("service.ftl");
+        //FreemarkerUtil.generator(servicePath + Domain + "Service.java", param);
+        gen(Domain, param, "service");
+        gen(Domain, param, "controller");
+    }
+
+    private static void gen(String Domain, Map<String, Object> param, String target) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig(target + ".ftl");
+        String toPath = serverPath + target + "/";
+        new File(toPath).mkdirs();
+        String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
+        String fileName = toPath + Domain + Target + ".java";
+        System.out.println("开始生成：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
--- member/src/main/java/top/wml/train/member/controller/PassengerController1.java
+++ member/src/main/java/top/wml/train/member/controller/PassengerController1.java
@@ -11,11 +11,9 @@ import top.wml.train.member.req.PassengerSaveReq;
 import top.wml.train.member.resp.PassengerQueryResp;
 import top.wml.train.member.service.PassengerService;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/passenger")
-public class PassengerController {
+public class PassengerController1 {
 
     @Resource
     private PassengerService passengerService;
