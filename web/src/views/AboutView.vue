ce8f8995c521f22fd5b52fedf7e0db96283ed1f3
ray <656482519@qq.com>
2023-11-21 14:14:15 +0800

    增加乘车人表，生成持久层代码

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -4,4 +4,28 @@
   </div>
   <a-button type="primary">测试</a-button>
   <plus-circle-outlined></plus-circle-outlined>
+
+   <a-dropdown :trigger="['click']">
+    <a class="ant-dropdown-link" @click.prevent>
+      BookType
+      <DownOutlined />
+    </a>
+    <template #overlay>
+      <a-menu>
+        <a-menu-item key="0">
+          <a href="http://www.alipay.com/">BookTypeEnum.WENXUELEI</a>
+        </a-menu-item>
+        <a-menu-item key="1">
+          <a href="http://www.taobao.com/">BookTypeEnum.YINGYUANLEI</a>
+        </a-menu-item>
+        <a-menu-divider />
+        <a-menu-item key="3">BookTypeEnum.ZHENGSHI</a-menu-item>
+      </a-menu>
+    </template>
+  </a-dropdown>
 </template>
+
+<script setup>
+import * as BookTypeEnum from "@/constant/BookTypeEnum";
+const BookTypeEnum = BookTypeEnum;
+</script>
