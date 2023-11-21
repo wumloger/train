a18ae9a26781294cb2820b9d56d6ab9419349051
ray <656482519@qq.com>
2023-11-21 15:01:59 +0800

    使用线程本地变量存储会员信息

---
+++ common/src/main/java/top/wml/train/common/resp/MemberLoginResp.java
@@ -0,0 +1,19 @@
+package top.wml.train.common.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberLoginResp {
+
+    private Long id;
+
+    private String mobile;
+
+    private String token;
+}
--- member/src/main/java/top/wml/train/member/service/PassengerService.java
+++ member/src/main/java/top/wml/train/member/service/PassengerService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import top.wml.train.common.context.LoginMemberContext;
 import top.wml.train.common.util.SnowUtil;
 import top.wml.train.member.domain.Passenger;
 import top.wml.train.member.mapper.PassengerMapper;
@@ -18,6 +19,7 @@ public class PassengerService {
     public void save(PassengerSaveReq req){
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req,Passenger.class);
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setId(SnowUtil.getSnowflakeNextId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
