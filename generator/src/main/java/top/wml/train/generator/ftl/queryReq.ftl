81b56b2813bae47ab598329ead0e3810610df0dd
ray <656482519@qq.com>
2023-11-21 19:47:40 +0800

    制作 queryReq.ftl 模版

---
+++ generator/src/main/java/top/wml/train/generator/ftl/queryReq.ftl
@@ -0,0 +1,12 @@
+package top.wml.train.${module}.req;
+
+import top.wml.train.common.req.PageReq;
+
+public class ${Domain}QueryReq extends PageReq {
+@Override
+public String toString() {
+return "${Domain}QueryReq{" +
+"} " + super.toString();
+}
+}
+}
--- member/src/main/java/top/wml/train/member/req/PassengerQueryReq.java
+++ member/src/main/java/top/wml/train/member/req/PassengerQueryReq.java
@@ -1,10 +1,23 @@
 package top.wml.train.member.req;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import top.wml.train.common.req.PageReq;
-@EqualsAndHashCode(callSuper = true)
-@Data
+
 public class PassengerQueryReq extends PageReq {
+
     private Long memberId;
+
+    public Long getMemberId() {
+    return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @Override
+public String toString() {
+return "PassengerQueryReq{" +
+"} " + super.toString();
+}
+}
 }
