0bd7a31ed00e8c444e98bd68cd2d923ccd2954da
ray <656482519@qq.com>
2023-11-23 14:47:51 +0800

    多节点场景中 quartz 的调度情况

---
+++ batch/src/main/java/top/wml/train/batch/job/Test2Job.java
@@ -0,0 +1,17 @@
+package top.wml.train.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+
+@DisallowConcurrentExecution
+public class Test2Job implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob222222 TEST开始");
+        System.out.println("TestJob222222 TEST结束");
+    }
+}
--- batch/src/main/java/top/wml/train/batch/job/Test3Job.java
+++ batch/src/main/java/top/wml/train/batch/job/Test3Job.java
@@ -8,19 +8,14 @@ import org.quartz.JobExecutionException;
 /**
  * @author mqxu
  * @date 2023/11/22
- * @description TestJob
+ * @description Test3Job
  **/
 @DisallowConcurrentExecution
-public class TestJob implements Job {
+public class Test3Job implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST 开始");
-        //try {
-        //    Thread.sleep(3000);
-        //} catch (InterruptedException e) {
-        //    e.printStackTrace();
-        //}
-        System.out.println("TestJob TEST 结束");
+        System.out.println("TestJob333333333 TEST开始");
+        System.out.println("TestJob333333333 TEST结束");
     }
 }
