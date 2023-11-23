236fd3e0f070bc30272ab0da4623100047c6de61
ray <656482519@qq.com>
2023-11-23 14:14:40 +0800

    使用数据库配置 quartz 调度任务

---
+++ batch/src/main/java/top/wml/train/batch/config/SchedulerConfig.java
@@ -0,0 +1,31 @@
+package top.wml.train.batch.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/**
+ * @author mqxu
+ * @date 2023/11/22
+ * @description SchedulerConfig
+ **/
+@Configuration
+public class SchedulerConfig {
+
+    @Resource
+    private MyJobFactory myJobFactory;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(@Qualifier("dataSource") DataSource dataSource) throws IOException {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setJobFactory(myJobFactory);
+        factory.setStartupDelay(2);
+        return factory;
+    }
+}
