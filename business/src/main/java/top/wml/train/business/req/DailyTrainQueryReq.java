271a97fa5a1b3f11b0cbd38ba7192bdc0519b604
ray <656482519@qq.com>
2023-11-24 08:43:46 +0800

    每日车次添加查询功能

---
+++ business/src/main/java/top/wml/train/business/req/DailyTrainQueryReq.java
@@ -0,0 +1,35 @@
+package top.wml.train.business.req;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import top.wml.train.common.req.PageReq;
+
+import java.util.Date;
+
+public class DailyTrainQueryReq extends PageReq {
+    private String code;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+    return "DailyTrainQueryReq{" +
+    "} " + super.toString();
+    }
+}
---
+++ business/src/main/java/top/wml/train/business/service/DailyTrainService.java
@@ -0,0 +1,77 @@
+package top.wml.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import top.wml.train.common.resp.PageResp;
+import top.wml.train.common.util.SnowUtil;
+import top.wml.train.business.domain.DailyTrain;
+import top.wml.train.business.domain.DailyTrainExample;
+import top.wml.train.business.mapper.DailyTrainMapper;
+import top.wml.train.business.req.DailyTrainQueryReq;
+import top.wml.train.business.req.DailyTrainSaveReq;
+import top.wml.train.business.resp.DailyTrainQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DailyTrainService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainService.class);
+
+    @Resource
+    private DailyTrainMapper dailyTrainMapper;
+
+    public void save(DailyTrainSaveReq req) {
+        DateTime now = DateTime.now();
+    DailyTrain dailyTrain = BeanUtil.copyProperties(req, DailyTrain.class);
+        if (ObjectUtil.isNull(dailyTrain.getId())) {
+        dailyTrain.setId(SnowUtil.getSnowflakeNextId());
+        dailyTrain.setCreateTime(now);
+        dailyTrain.setUpdateTime(now);
+            dailyTrainMapper.insert(dailyTrain);
+        } else {
+        dailyTrain.setUpdateTime(now);
+            dailyTrainMapper.updateByPrimaryKey(dailyTrain);
+        }
+    }
+
+    public PageResp<DailyTrainQueryResp> queryList(DailyTrainQueryReq req) {
+        DailyTrainExample dailyTrainExample = new DailyTrainExample();
+        dailyTrainExample.setOrderByClause("date desc,code asc");
+        dailyTrainExample.setOrderByClause("id desc");
+        DailyTrainExample.Criteria criteria = dailyTrainExample.createCriteria();
+        if(ObjectUtil.isNotNull(req.getDate())){
+            criteria.andDateEqualTo(req.getDate());
+        }
+        if(ObjectUtil.isNotEmpty(req.getCode())){
+            criteria.andCodeEqualTo(req.getCode());
+        }
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<DailyTrain> dailyTrainList = dailyTrainMapper.selectByExample(dailyTrainExample);
+
+        PageInfo<DailyTrain> pageInfo = new PageInfo<>(dailyTrainList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<DailyTrainQueryResp> list = BeanUtil.copyToList(dailyTrainList, DailyTrainQueryResp.class);
+
+        PageResp<DailyTrainQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        dailyTrainMapper.deleteByPrimaryKey(id);
+    }
+
+}
