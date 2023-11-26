6cedadd82cbb527a469cfb41b7d286c6ba2d7240
ray <656482519@qq.com>
2023-11-26 17:40:49 +0800

    为余票信息页面增加查询条件

--- business/src/main/java/top/wml/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/top/wml/train/business/req/DailyTrainTicketQueryReq.java
@@ -1,8 +1,24 @@
 package top.wml.train.business.req;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 import top.wml.train.common.req.PageReq;
 
+import java.util.Date;
+
+@Getter
+@Setter
 public class DailyTrainTicketQueryReq extends PageReq {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String trainCode;
+
+    private String start;
+
+    private String end;
+
     @Override
     public String toString() {
     return "DailyTrainTicketQueryReq{" +
